c975ed95579ca537fcf072595d8fe09075ee4e1b
amntllhz <amanatullah1904@gmail.com>
2025-05-26 19:50:42 +0700

    comp: responsive navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (        
-        <div className="fixed top-4 left-1/2 transform -translate-x-1/2 w-full max-w-xl bg-primary/30 backdrop-blur-md z-50 shadow-lg border border-accent1 text rounded-full px-6 py-3">
+        <div className="fixed top-4 left-1/2 transform -translate-x-1/2 w-full bg-primary/30 backdrop-blur-md z-50 shadow-lg border border-accent1 text rounded-full px-6 py-2.5 max-w-xl xs:max-w-72 sm:max-w-80 md:max-w-sm lg:max-w-sm">
             <div className="flex items-center justify-between">
                 <p className="font-bold text-accent4">AM</p>
                 <div className="flex gap-4">
-                <Link className="font-medium text-accent4 hover:text-accent3 transition duration-300 ease-in-out">Home</Link>
-                <Link className="font-medium text-accent4 hover:text-accent3 transition duration-300 ease-in-out">About</Link>
-                <Link className="font-medium text-accent4 hover:text-accent3 transition duration-300 ease-in-out">Projects</Link>
+                <Link className="font-medium text-accent4 xs:text-xs sm:text-sm md:text-md hover:text-accent3 transition duration-300 ease-in-out">Home</Link>
+                <Link className="font-medium text-accent4 xs:text-xs sm:text-sm md:text-md hover:text-accent3 transition duration-300 ease-in-out">About</Link>
+                <Link className="font-medium text-accent4 xs:text-xs sm:text-sm md:text-md hover:text-accent3 transition duration-300 ease-in-out">Projects</Link>
                 </div>
                 <p className="font-bold text-accent4">AM</p>
             </div>
