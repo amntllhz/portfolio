ce71c57f542b678fc2c5159e133adc5ccf2acc30
amntllhz <amanatullah1904@gmail.com>
2025-06-05 11:22:24 +0700

    sect: project sect micro fixing

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -55,8 +55,8 @@ const Projects = () => {
 
     return (
         <>            
-
-            <div className="flex flex-col items-center mb-10">
+          <div className="relative sm:top-40 xs:-top-40">
+            <div className="relative flex flex-col items-center mb-10">
                   <h1 className="flex font-display mb-4 text-center font-semibold text-accent4 xs:text-2xl sm:text-4xl">Some of My Projects</h1>
                   <p className="text-accent4 font-display text-center sm:text-sm xs:text-xs xs:max-w-xs">Some of the projects I have worked on is a Web development, UI Design, and Design Graphic</p>
             </div>
@@ -85,6 +85,7 @@ const Projects = () => {
                 </div>
               ))}
             </div>
+          </div>
         </>
     )
 
