1215a77658608f9ae04a0c30cbb7e56d1dd5afc1
amntllhz <amanatullah1904@gmail.com>
2025-06-06 17:25:33 +0700

    sect: projects section 80%

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,37 +1,78 @@
 import { HoverBorderGradient } from "../HoverBorderGradient/HoverBorderGradient"
 import { HiEye } from "react-icons/hi"
 import projectList from "../../data/projectList"
+import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { useState } from "react"
+import { HiX } from "react-icons/hi";
 
-const ProjectCard = () => {       
+const ProjectCard = () => {     
+    const [isOpenIndex, setIsOpenIndex] = useState(null)
 
     return (
         <>
-        <div className="grid grid-cols-1 sm:grid-cols-3 justify-center sm:max-w-5xl mx-auto text-center gap-4 w-full xs:max-w-9/10">
+          <div className="grid grid-cols-1 sm:grid-cols-3 justify-center sm:max-w-5xl mx-auto text-center gap-4 w-full xs:max-w-9/10">
 
-              {projectList.map((item, index) => (
-                <div key={index}>
-                  <HoverBorderGradient
-                    containerClassName="rounded-2xl w-full"
-                    as="div"
-                    className="relative justify-center items-center space-x-2 w-full"
-                  >
-                    <div className="flex mt-2 justify-between w-full px-6">
-                      <p className="font-display text-sm font-semibold text-accent4">Project.io</p>
-                      <button onClick={() => setOpenModal(true)} className="flex items-center gap-1 group cursor-pointer">
-                        <HiEye className="text-accent4 group-hover:text-accent3 transition duration-300" />
-                        <p className="font-display text-sm font-semibold text-accent4 group-hover:text-accent3 transition duration-300">View</p>
-                      </button>
-                    </div>
-                    
-                    <div className="relativerounded-xl p-3">
-                      {item.content}
-                    </div>
-                  </HoverBorderGradient>
-                </div>
-              ))}
-              
-                
-        </div>
+                {projectList.map((item, index) => (
+                  <div key={index}>
+                    <HoverBorderGradient
+                      containerClassName="rounded-2xl w-full"
+                      as="div"
+                      className="relative justify-center items-center space-x-2 w-full"
+                    >
+                      <div className="flex mt-2 justify-between w-full px-6">
+                        <p className="font-display text-sm font-semibold text-accent4">Project.io</p>
+                        <button onClick={() => setIsOpenIndex(index)} className="flex items-center gap-1 group cursor-pointer">
+                          <HiEye className="text-accent4 group-hover:text-accent3 transition duration-300" />
+                          <p className="font-display text-sm font-semibold text-accent4 group-hover:text-accent3 transition duration-300">View</p>
+                        </button>
+                      </div>
+                      
+                      <div className="relative rounded-xl p-3">
+                        {item.content}
+                      </div>
+
+                      <Dialog open={isOpenIndex === index} onClose={() => setIsOpenIndex(null)} className="relative z-50">
+                        <div className="fixed inset-0 flex w-screen items-center justify-center p-4 bg-deep/20 backdrop-blur-xs">
+                          <DialogPanel className="sm:h-fit xs:h-screen xs:w-screen xs:rounded-none sm:max-w-3/4 space-y-4 bg-accent4 p-4 sm:rounded-xl xs:overflow-clip">
+                            <DialogTitle className="text-xl relative font-bold font-display text-primary">
+                              <div className="flex justify-between items-center w-full">
+                                <p className=" flex font-display font-extrabold text-primary">{item.title}</p>
+                                <button onClick={() => setIsOpenIndex(null)} className="flex cursor-pointer hover:bg-primary/10 px-1.5 py-1.5 rounded-md transition duration-100">
+                                  <HiX className="text-primary" />
+                                </button>                              
+                              </div>
+                            </DialogTitle>
+
+                            <Description className="flex sm:h-96 xs:h-[calc(100vh-5rem)] gap-4 w-full xs:flex-col sm:flex-row xs:overflow-y-auto">
+                              <div className="flex flex-col sm:overflow-y-auto xs:overflow-visible p-4 rounded-md bg-white gap-2 sm:basis-2/5 xs:basis-full">
+                                <p className="font-display text-primary text-sm text-justify">{item.desc}</p>
+                                <p className="font-display text-primary font-bold text-sm text-justify">Feature :</p>
+                                <p className="font-display text-primary text-sm text-justify">{item.feature}</p>                                                          
+                                <p className="font-display text-primary font-bold text-sm text-justify">Tech Stack :</p>
+                                <div className="font-display  flex gap-2 flex-wrap text-sm text-justify">
+                                  {item.techStack.map((tech, index) => (
+                                    <div key={index}>
+                                      <p className="font-display text-primary font-semibold text-sm py-1 px-2 w-fit bg-accent3/10 rounded-lg">{tech}</p>
+                                    </div>
+                                  ))}
+                                </div>                                                          
+                              </div>
+                              <div className="flex flex-col sm:overflow-y-auto xs:overflow-visible p-4 rounded-md bg-white gap-2 xs:gap-y-4 sm:basis-3/5 xs:basis-full">
+                                <p>{item.content}</p>
+                                <p>{item.content}</p>
+                                <p>{item.content}</p>
+                              </div>
+                            </Description>                            
+                            
+                          </DialogPanel>
+                        </div>
+                      </Dialog>
+                    </HoverBorderGradient>
+                  </div>
+                                                      
+                ))}
+                                
+          </div>
         </>
     )
 }
--- src/data/projectList.jsx
+++ src/data/projectList.jsx
@@ -1,7 +1,10 @@
 
 const projectList = [
-    {
-      title: "2016",
+    {       
+      title: "Project1",
+      desc : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex recusandae fuga quasi nesciunt obcaecati rerum quam, placeat quaerat in qui, eaque eius? ",
+      feature : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex recusandae fuga quasi nesciunt obcaecati rerum quam, placeat quaerat in qui, eaque eius? A veritatis sed cupiditate beatae sint excepturi necessitatibus accusamus ad dignissimos non fugiat neque porro aliquid doloribus, tempore maxime commodi dolorem incidunt odio. Officia illo reprehenderit quia dicta!Jl. Raya Cepagan, Karangjati, Cepagan, Kec. Warungasem, Kabupaten Batang, Jawa Tengah 51252",
+      techStack : ["HTML", "CSS", "JavaScript", "React", "TailwindCSS"],
       content: (
         <div>                    
           <div>            
@@ -16,9 +19,12 @@ const projectList = [
       ),
     },
     {
-      title: "2019",
+      title: "Project2",
+      desc : "State Junior High School 2 Warungasem",
+      feature : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex recusandae fuga quasi nesciunt obcaecati rerum quam, placeat quaerat in qui, eaque eius? A veritatis sed cupiditate beatae sint excepturi necessitatibus accusamus ad dignissimos non fugiat neque porro aliquid doloribus, tempore maxime commodi dolorem incidunt odio. Officia illo reprehenderit quia dicta!Jl. Raya Cepagan, Karangjati, Cepagan, Kec. Warungasem, Kabupaten Batang, Jawa Tengah 51252",
+      techStack : ["HTML", "CSS", "JavaScript", "React", "TailwindCSS"],
       content: (
-        <div>                  
+        <div>                    
           <div>            
             <img
               src="https://assets.aceternity.com/templates/startup-2.webp"
@@ -31,20 +37,23 @@ const projectList = [
       ),
     },
     {
-      title: "2019",
+      title: "Project3",
+      desc : "State Junior High School 3 Warungasem",
+      feature : "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex recusandae fuga quasi nesciunt obcaecati rerum quam, placeat quaerat in qui, eaque eius? A veritatis sed cupiditate beatae sint excepturi necessitatibus accusamus ad dignissimos non fugiat neque porro aliquid doloribus, tempore maxime commodi dolorem incidunt odio. Officia illo reprehenderit quia dicta!Jl. Raya Cepagan, Karangjati, Cepagan, Kec. Warungasem, Kabupaten Batang, Jawa Tengah 51252",
+      techStack : ["HTML", "CSS", "JavaScript", "React", "TailwindCSS"],
       content: (
-        <div>                  
+        <div>                    
           <div>            
             <img
               src="https://assets.aceternity.com/templates/startup-2.webp"
               alt="startup template"
               width={500}
               height={500}
-              className="h-20 xs:h-56 w-full rounded-lg object-cover relative shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />                        
+              className="h-20 xs:h-56 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60" />                        
           </div>
         </div>
       ),
-    },
+    },    
 ];
 
 export default projectList
