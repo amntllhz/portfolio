a00df9d3b5c57c6d1bc9273f27f824a5ebdd63b7
amntllhz <amanatullah1904@gmail.com>
2025-06-07 21:27:37 +0700

    feat: contact navigation

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -17,9 +17,15 @@ const Contact = () => {
                     <h1 className="font-display text-center font-bold text-accent4 xs:text-2xl sm:text-4xl">Start Big Collaborate</h1>
                     <p className="text-accent4 font-display text-center sm:max-w-md sm:text-sm xs:text-xs xs:max-w-xs">I am interested in freelance opportunities.if you have other requests or questions, don't hesitate to contact me.</p>                
                     <div className="flex flex-row items-center justify-center bg-accent1/30 rounded-full px-2.5 py-2 mt-4 sm:gap-x-3 xs:gap-x-1.5">
-                        <Button icon={<SiGithub />} bgButton="bg-gradient-to-r from-accent1/30 via-accent3/40 to-accent1/30 " text="Github" />
-                        <Button icon={<SiLinkedin />} bgButton="bg-gradient-to-r from-accent1/30 via-accent3/40 to-accent1/30 " text="LinkedIn" />
-                        <Button icon={<SiGmail />} bgButton="bg-gradient-to-r from-accent1/30 via-accent3/40 to-accent1/30 " text="Email" />
+                        <a href="https://github.com/amntllhz" target="_blank" rel="noopener noreferrer">
+                            <Button icon={<SiGithub />} bgButton="bg-gradient-to-r from-accent1/30 via-accent3/40 to-accent1/30 " text="Github" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/aldiamn" target="_blank" rel="noopener noreferrer">
+                            <Button icon={<SiLinkedin />} bgButton="bg-gradient-to-r from-accent1/30 via-accent3/40 to-accent1/30 " text="LinkedIn" />
+                        </a>
+                        <a href="mailto:amanatullah1904@gmail.com">                            
+                            <Button icon={<SiGmail />} bgButton="bg-gradient-to-r from-accent1/30 via-accent3/40 to-accent1/30 " text="Email" />
+                        </a>
                     </div> 
                 </div>
                 <div className="flex items-center justify-center gap-4 sm:max-w-5xl xs:max-w-9/10 mx-auto mb-6">
